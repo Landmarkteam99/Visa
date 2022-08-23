3293ddb4b727b28f3aa23f49581bc081dc031427
Philcado995 <Philcado995@gmail.com>
2022-08-22 21:13:54 -0400

    Deployment version 2

--- deploy.py
+++ deploy.py
@@ -1,3 +1,7 @@
 # Deploying a python web application
-echo "Hello World"   = shell scripts
-python('Hello World') = python script
+#echo "Hello World"
+#= shell scripts
+#python ('Hello World')
+#= python script
+
+print('Hello World')
